8f71a33eb1db6351995cae8f8bb2fb977aab2f94
Maanuj Vora <maanujvora@gmail.com>
2020-08-12 23:38:24 -0700

    Update tabulate.py

--- data/tabulate.py
+++ data/tabulate.py
@@ -25,11 +25,13 @@ while index < len(csvList):
             data[str(lineIndex)] = item
             lineIndex+=1
 
+    path = os.path.dirname(__file__)
+    path = path[:-16]
     try:
-        f = open(jsonList[index], "w")
+        f = open(path+jsonList[index], "w")
     except Exception as e:
-        f = open(jsonList[index], "x") 
-        
+        f = open(path+jsonList[index], "x") 
+
     with open(jsonList[index], 'w', encoding='utf-8') as openJSON:
         openJSON.write(json.dumps(data, indent=4, ensure_ascii=False))
 

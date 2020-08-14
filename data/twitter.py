821c443e7258fdc7acb707cf0a88b9ebc42fb4da
Maanuj Vora <maanujvora@gmail.com>
2020-08-13 17:43:13 -0700

    Update twitter.py

--- data/twitter.py
+++ data/twitter.py
@@ -10,8 +10,8 @@ csvFilePath = default.get("apiMethods/data.json").csvPath
 
 def getTodayTweets(screen_name):
 
-    auth = tweepy.OAuthHandler(process.env.consumer_key, process.env.consumer_secret)
-    auth.set_access_token(process.env.access_key, process.env.access_secret)
+    auth = tweepy.OAuthHandler(os.getenv['consumer_key'], os.getenv['consumer_secret'])
+    auth.set_access_token(os.getenv['access_key'], os.getenv['access_secret'])
     api = tweepy.API(auth)
 
     startDate = (datetime.today() - timedelta(days=2)).replace(hour=0, minute=0, second=0)

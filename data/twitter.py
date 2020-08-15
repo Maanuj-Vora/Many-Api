d0a25864b11064aa75779000f2e1d10ff4787390
Maanuj Vora <maanujvora@gmail.com>
2020-08-14 17:48:23 -0700

    Update twitter.py

--- data/twitter.py
+++ data/twitter.py
@@ -38,6 +38,8 @@ def getTodayTweets(screen_name):
     outtweets = [[tweet.id_str, tweet.created_at, tweet.full_text]
                  for tweet in tweets]
 
+    file = open(f'{csvFilePath}{screen_name}_tweets.csb', "w+")
+
     shouldRow = False
     with open(f'{csvFilePath}{screen_name}_tweets.csv', 'r') as check:
         char = check.read(1)

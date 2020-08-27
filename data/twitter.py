d89d208d2d763493365d8aae4a1e3351cb1d5d7a
Maanuj Vora <maanujvora@gmail.com>
2020-08-27 10:52:38 -0700

    Update twitter.py

--- data/twitter.py
+++ data/twitter.py
@@ -47,7 +47,7 @@ def getTodayTweets(screen_name):
 
 
     tweets = []
-    tmpTweets = api.user_timeline(screen_name)
+    tmpTweets = api.user_timeline(screen_name, count=300)
     for tweet in tmpTweets:
         if tweet.created_at < endDate and tweet.created_at > startDate:
             tweets.append(tweet)

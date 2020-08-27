a48d7e713722334e20b8454b78f457052ca689e3
Maanuj Vora <maanujvora@gmail.com>
2020-08-27 10:38:12 -0700

    Update twitter.py

--- data/twitter.py
+++ data/twitter.py
@@ -32,13 +32,16 @@ def getTodayTweets(screen_name):
             tweets.append(tweet)
 #     print(tmpTweets)
     if(len(tmpTweets) > 1):
-#         while (tmpTweets[-1].created_at > startDate):
-        while (tmpTweets[len(tmpTweets)-1].created_at > startDate):
-            tmpTweets = api.user_timeline(screen_name, max_id=tmpTweets[-1].id, tweet_mode="extended")
-#                 screen_name, max_id=tmpTweets[-1].id, tweet_mode="extended")
-            for tweet in tmpTweets:
-                if tweet.created_at < endDate and tweet.created_at > startDate:
-                    tweets.append(tweet)
+        try:
+            while (tmpTweets[-1].created_at > startDate):
+                tmpTweets = api.user_timeline(screen_name, max_id=tmpTweets[-1].id, tweet_mode="extended")
+    #                 screen_name, max_id=tmpTweets[-1].id, tweet_mode="extended")
+                for tweet in tmpTweets:
+                    if tweet.created_at < endDate and tweet.created_at > startDate:
+                        tweets.append(tweet)
+         except Exception as e:
+            print('Error: ' + e)
+            
     else:
         return
 

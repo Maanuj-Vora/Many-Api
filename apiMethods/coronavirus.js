2288018da2101f0b1892c3da8d8d41b5e766199f
Maanuj Vora <maanujvora@gmail.com>
2020-09-02 17:36:23 -0700

    Update coronavirus.js

--- apiMethods/coronavirus.js
+++ apiMethods/coronavirus.js
@@ -70,7 +70,7 @@ function getDataLocal(values, date, iso) {
     data = []
 
     if (date == undefined && iso == undefined) {
-        var today = new Date().toLocaleString("en-US", {timeZone: "America/New_York"});
+        var today = new Date();
         var dd = String(today.getDate()).padStart(2, '0');
         var mm = String(today.getMonth() + 1).padStart(2, '0');
         var yyyy = today.getFullYear();
